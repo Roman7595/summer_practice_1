ad08253740abbe87d59e5a68a95feb8fe6eb2938
Roman7595 <126889828+Roman7595@users.noreply.github.com>
2024-08-27 02:40:41 +0300

    refactor auto repository

--- src/main/java/org/example/practice/repositories/impl/AutoRepositoryImpl.java
+++ src/main/java/org/example/practice/repositories/impl/AutoRepositoryImpl.java
@@ -1,9 +1,10 @@
 package org.example.practice.repositories.impl;
 
 import org.example.practice.entities.Auto;
+import org.example.practice.repositories.CRUDRepositories.GetRepository;
+import org.example.practice.repositories.CRUDRepositories.SaveRepository;
 import org.example.practice.repositories.interfaces.AutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,21 +13,22 @@ import java.util.Optional;
 @Repository
 public class AutoRepositoryImpl implements AutoRepository {
     @Autowired
-    private JpaRepository<Auto, Integer> autoJpaRepository;
-
+    private SaveRepository<Auto> saveRepository;
+    @Autowired
+    private GetRepository<Auto> getRepository;
 
     @Override
     public Auto save(Auto auto) {
-        return autoJpaRepository.save(auto);
+        return saveRepository.save(auto);
     }
 
     @Override
     public Optional<Auto> findById(int id){
-        return autoJpaRepository.findById(id);
+        return getRepository.findById(id, "Auto");
     }
 
     @Override
     public List<Auto> getAll(){
-        return autoJpaRepository.findAll();
+        return getRepository.findAll("Auto");
     }
 }
