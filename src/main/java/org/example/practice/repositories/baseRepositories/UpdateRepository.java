870f360ed73a13643e01f879c46ad0d326381e5b
Roman7595 <126889828+Roman7595@users.noreply.github.com>
2024-08-29 01:19:35 +0300

    add update repository

--- src/main/java/org/example/practice/repositories/baseRepositories/UpdateRepository.java
+++ src/main/java/org/example/practice/repositories/baseRepositories/UpdateRepository.java
@@ -1,2 +1,5 @@
-package org.example.practice.repositories.baseRepositories;public interface UpdateRepository {
+package org.example.practice.repositories.baseRepositories;
+
+public interface UpdateRepository<T> {
+    public T update(T entity);
 }
--- src/main/java/org/example/practice/repositories/baseRepositories/UpdateRepositoryImpl.java
+++ src/main/java/org/example/practice/repositories/baseRepositories/UpdateRepositoryImpl.java
@@ -1,2 +1,19 @@
-package org.example.practice.repositories.baseRepositories;public class UpdateRepositoryImpl {
+package org.example.practice.repositories.baseRepositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UpdateRepositoryImpl<T> implements UpdateRepository<T> {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public T update(T entity) {
+        entityManager.merge(entity);
+        return entity;
+    }
 }
