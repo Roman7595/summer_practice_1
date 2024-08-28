03e661fb1f1b79ebc9ad142f8ecada61e51ddcde
Roman7595 <126889828+Roman7595@users.noreply.github.com>
2024-08-29 00:53:15 +0300

    refactor base repos

--- src/main/java/org/example/practice/repositories/impl/AutoRepositoryImpl.java
+++ src/main/java/org/example/practice/repositories/impl/AutoRepositoryImpl.java
@@ -1,8 +1,8 @@
 package org.example.practice.repositories.impl;
 
 import org.example.practice.entities.Auto;
-import org.example.practice.repositories.CRUDRepositories.GetRepository;
-import org.example.practice.repositories.CRUDRepositories.SaveRepository;
+import org.example.practice.repositories.baseRepositories.GetRepository;
+import org.example.practice.repositories.baseRepositories.SaveRepository;
 import org.example.practice.repositories.interfaces.AutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -24,11 +24,11 @@ public class AutoRepositoryImpl implements AutoRepository {
 
     @Override
     public Optional<Auto> findById(int id){
-        return getRepository.findById(id, "Auto");
+        return getRepository.findById(id, Auto.class);
     }
 
     @Override
     public List<Auto> getAll(){
-        return getRepository.findAll("Auto");
+        return getRepository.findAll(Auto.class);
     }
 }
